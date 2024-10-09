36b85c12f934993e24a10519ac6212e78e45fa9c
47813 <vanguard0956@gmail.com>
2024-10-09 13:44:33 +0900

    Changed API Settings to make sure every data is reachable

--- kpaas_flutter/lib/apiconnectiontest/data_controller.dart
+++ kpaas_flutter/lib/apiconnectiontest/data_controller.dart
@@ -2,29 +2,52 @@ import 'package:get/get.dart';
 import 'api_service.dart';
 
 class DataController extends GetxController {
-  var isLoading = true.obs; // 로딩 상태 관리
-  var dataList = [].obs; // 데이터 리스트 관리
+  var isLoading = true.obs;
+  var market_bond_issue_info = [].obs;
+  var market_bond_inquire_daily_price = [].obs;
+  var news = [].obs;
   final ApiService apiService = ApiService();
 
   @override
   void onInit() {
-    fetchData(); // 초기화 시 데이터 불러오기
     super.onInit();
+    fetchData();
   }
 
-  void fetchData() async {
+  Future<void> fetchData() async {
+    isLoading(true);
     try {
-      isLoading(true);
-      var response = await apiService.fetchData('http://10.0.2.2:8000/api/koreaib/news/');
-      if (response.statusCode == 200) {
-        dataList.assignAll(response.data);
+      final responses = await Future.wait([
+        apiService.fetchData('http://10.0.2.2:8000/api/koreaib/market-bond-issue-info/market_bond_issue_info/?code=KR6150351E98'),
+        apiService.fetchData('http://10.0.2.2:8000/api/koreaib/market-bond-inquire-daily-price/market_bond_issue_info/?code=KR6150351E98'),
+        apiService.fetchData('http://10.0.2.2:8000/api/koreaib/news/data/?query=%EC%82%BC%EC%84%B1&sort=date')
+      ]);
+
+      final response1 = responses[0];
+      final response2 = responses[1];
+      final response3 = responses[2];
+
+      if (response1.statusCode == 200 && response2.statusCode == 200 && response3.statusCode == 200) {
+        market_bond_issue_info.clear();
+        market_bond_inquire_daily_price.clear();
+        news.clear();
+
+        if (response1.data is Map) {
+          market_bond_issue_info.add(response1.data);
+        }
+        if (response2.data is List) {
+          market_bond_inquire_daily_price.assignAll(response2.data.map((item) => item['fields']).toList());
+        }
+        if (response3.data is Map) {
+          news.assignAll(response3.data['items']);
+        }
       } else {
-        print('Failed to load data');
+        print('Failed to load data from one or more endpoints');
       }
     } catch (e) {
       print('Error: $e');
     } finally {
-      isLoading(false); // 로딩 완료
+      isLoading(false);
     }
   }
 }
