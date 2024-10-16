19333c9f764f0065b329b213d434500ec7501de8
Muon05 <dbfugks@naver.com>
2024-10-16 15:00:14 +0900

    feat: env, kakao JWT

--- kpaas_task/config/celery.py
+++ kpaas_task/config/celery.py
@@ -3,9 +3,9 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kpaas_task.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-app = Celery('kpaas_task')
+app = Celery('config')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
