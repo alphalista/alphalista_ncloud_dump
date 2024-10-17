62a4dcc08c845969f42c9f865fc442b3c175b71c
sungminna <sungmin.na330@gmail.com>
2024-10-18 00:05:21 +0900

    feat: add pagenation and throttling

--- kpaas_rest/kpaas_rest/settings.py
+++ kpaas_rest/kpaas_rest/settings.py
@@ -140,5 +140,15 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day',
+    }
 }
