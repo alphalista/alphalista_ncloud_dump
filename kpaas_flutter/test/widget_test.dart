e16bc8f70f89ad6b70471e5e115c19ceebc19c2d
47813 <vanguard0956@gmail.com>
2024-10-26 16:38:10 +0900

    Changed starting-page for Debug session

--- kpaas_flutter/test/widget_test.dart
+++ kpaas_flutter/test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:kpaas_flutter/first.dart';
 
 import 'package:kpaas_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(TabNavigationExample());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
