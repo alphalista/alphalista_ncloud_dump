629a1de850b264169841d1c6d33dc53ad4e64bb0
YimTaeKeun <ytk030305@gmail.com>
2024-11-02 20:01:46 +0900

    [FIX]: Fixed Holding Models

--- kpaas_rest/crawled_OtcBond/models.py
+++ kpaas_rest/crawled_OtcBond/models.py
@@ -19,5 +19,8 @@ class OTC_Bond_Interest(models.Model):
 class OTC_Bond_Holding(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     bond_code = models.ForeignKey(OTC_Bond, on_delete=models.CASCADE)
+    price_per_10 = models.CharField(max_length=100) # 1만원 채권당 매수단가
+    quantity = models.CharField(max_length=100)
+    purchase_date = models.CharField(max_length=100)
     class Meta:
         unique_together = ('user_id', 'bond_code')
